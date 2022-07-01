6f1c1de1f861d39546a183f46a724ad4e4859a40
calebcheptumo <ckibusia@gmail.com>
2022-07-01 11:59:51 -0400

    create models

--- workers/models.py
+++ workers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    designation = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    age = models.IntegerField()

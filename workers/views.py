d5e758cefe94e123b1cb4083bf8839c20bee5780
calebcheptumo <ckibusia@gmail.com>
2022-07-01 13:20:45 -0400

    create templates

--- workers/views.py
+++ workers/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render ,get_list_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Employee
 
 
@@ -17,11 +17,33 @@ def edit(request, worker_id):
 
 def delete(request, worker_id):
     employee = get_list_or_404(Employee, pk=worker_id)
-    return render(request, 'workers/delete.html', {'employee': employee})
+    employee.delete()
+    return render(request, '/workers/', {'employee': employee})
+
+def update(request):
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    designation = request.POST['designation']
+    email = request.POST['email']
+    phone = request.POST['phone']
+    address = request.POST['address']
+    city = request.POST['city']
+    state = request.POST['state']
+    age = request.POST['age']
+    employee = Employee(first_name=first_name, last_name=last_name, designation=designation, email=email, phone=phone, address=address, city=city, state=state, age=age)
+    employee.save()
+    return HttpResponseRedirect( '/workers/')
 
 def insert(request):
-    return render(request, 'workers/insert.html')
-
-def update(request, worker_id):
-    employee = get_list_or_404(Employee, pk=worker_id)
-    return render(request, 'workers/update.html', {'employee': employee})
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    designation = request.POST['designation']
+    email = request.POST['email']
+    phone = request.POST['phone']
+    address = request.POST['address']
+    city = request.POST['city']
+    state = request.POST['state']
+    age = request.POST['age']
+    employee = Employee(first_name=first_name, last_name=last_name, designation=designation, email=email, phone=phone, address=address, city=city, state=state, age=age)
+    employee.save()
+    return HttpResponseRedirect( '/workers/')
